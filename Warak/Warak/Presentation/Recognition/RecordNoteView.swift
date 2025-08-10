16e1c3af1eaeeecbc70d1f98d47e8dfcece49e92
0tak2 <0tak2.code@gmail.com>
2025-08-10 17:07:26 +0900

    Feat: 음성인식 언어 선택

---
+++ Warak/Warak/Presentation/Recognition/RecordNoteView.swift
@@ -0,0 +1,111 @@
+//
+//  RecordNoteView.swift
+//  Warak
+//
+//  Created by 임영택 on 8/10/25.
+//
+
+import SwiftUI
+import os.log
+
+struct RecordNoteView: View {
+    @State private var speechRecognitionModel = SpeechRecognitionModel()
+    @State private var animationTimer: Timer?
+    @State private var direction: AnimationDirection = .decrease
+    @State private var buttonPadding: CGFloat = 64
+    @State private var showConfirmAlert = false
+    
+    @Binding var navigationModel: NavigationModel
+    
+    @Environment(\.modelContext) private var modelContext
+    
+    private let logger = Logger.category("RecordNoteView")
+    
+    private struct Constants {
+        static let buttonInitialPadding: CGFloat = 128
+        static let buttonMaxPadding: CGFloat = 128
+        static let buttonMinPadding: CGFloat = 0
+        static let buttonAnimationWeights: CGFloat = 4
+    }
+    
+    var body: some View {
+        VStack {
+            if speechRecognitionModel.isRecording {
+                Text(speechRecognitionModel.recognizedText)
+            }
+            
+            if !speechRecognitionModel.isRecording {
+                Picker("인식 언어", selection: $speechRecognitionModel.recognizingLanguage) {
+                    ForEach(speechRecognitionModel.supportedLanguages, id: \.self) { language in
+                        Text(language.displayName)
+                    }
+                }
+                .pickerStyle(.menu)
+            }
+            
+            Spacer()
+            
+            Button {
+                recordButtonTapped()
+                if !speechRecognitionModel.isRecording {
+                    showConfirmAlert.toggle()
+                }
+            } label: {
+                Circle()
+                    .foregroundStyle(Color.red)
+            }
+            .padding(.all, buttonPadding)
+            
+            Spacer()
+        }
+        .alert("저장할까요?", isPresented: $showConfirmAlert) {
+            Button("저장") {
+            }
+            Button("다시 녹음하기") { }
+        }
+    }
+}
+
+// MARK: User Intents
+extension RecordNoteView {
+    func recordButtonTapped() {
+        if !speechRecognitionModel.isRecording {
+            animationTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+                withAnimation {
+                    if buttonPadding <= Constants.buttonMinPadding {
+                        direction = .increase
+                    }
+                    
+                    if buttonPadding >= Constants.buttonMaxPadding {
+                        direction = .decrease
+                    }
+                    
+                    buttonPadding += direction.delta * Constants.buttonAnimationWeights
+                }
+            }
+        } else {
+            self.animationTimer?.invalidate()
+            self.animationTimer = nil
+        }
+        
+        speechRecognitionModel.triggerButtonTapped()
+    }
+}
+
+fileprivate enum AnimationDirection {
+    case increase
+    case decrease
+    
+    var delta: CGFloat {
+        switch self {
+        case .increase:
+            return 1
+        case .decrease:
+            return -1
+        }
+    }
+}
+
+#Preview {
+    RecordNoteView(navigationModel: .constant(NavigationModel()))
+}
--- Warak/Warak/Presentation/Recognition/SpeechRecognitionModel.swift
+++ Warak/Warak/Presentation/Recognition/SpeechRecognitionModel.swift
@@ -9,10 +9,12 @@ import Foundation
 import Combine
 
 @Observable
-class RecordNoteViewModel {
+class SpeechRecognitionModel {
     private(set) var isRecording = false
     private(set) var recognizedText = ""
     private var cancellables: Set<AnyCancellable> = []
+    var recognizingLanguage = Languages.ko_KR
+    let supportedLanguages = Languages.allCases
     
     let recognizer = SpeechRecognizer()
     
@@ -24,8 +26,9 @@ class RecordNoteViewModel {
         recognizer.recognizedTextSubject
             .receive(on: RunLoop.main)
             .sink { [weak self] (text, isFinal) in
-                self?.recognizedText = text
-                print("recognizedText: \(text)")
+                if !text.isEmpty {
+                    self?.recognizedText = text
+                }
                 
                 if isFinal {
                     Task { @MainActor in // isFinal 방출 시점에는 오디오 세션 정리 전이므로, 다음 태스크로 스케쥴링
@@ -38,7 +41,7 @@ class RecordNoteViewModel {
     
     func startRecording() {
         isRecording = true
-        recognizer.startRecording()
+        recognizer.startRecording(languageCode: recognizingLanguage.rawValue)
     }
     
     func stopRecording() {
@@ -47,12 +50,26 @@ class RecordNoteViewModel {
     }
 }
 
-extension RecordNoteViewModel {
-    func circleButtonTapped() {
+extension SpeechRecognitionModel {
+    func triggerButtonTapped() {
         if !isRecording {
             startRecording()
         } else {
             stopRecording()
+        }
+    }
+}
+
+enum Languages: String, CaseIterable {
+    case ko_KR
+    case en_US
+    
+    var displayName: String {
+        switch self {
+        case .ko_KR:
+            return "한국어"
+        case .en_US:
+            return "영어"
         }
     }
 }
